a6bfec2c8e6c6615cb91e0721e3b4203215cc98f
lasseraty <raty.lasse@gmail.com>
2026-07-13 22:51:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = "https://gukdfaervxkiwdawvnsx.supabase.com";
+export const SUPABASE_URL = "https://gukdfaervxkiwdawvnsx.supabase.co";
 export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable__owZx1LPEQA0bSEErTv-Ng_yHQN_jxa";
